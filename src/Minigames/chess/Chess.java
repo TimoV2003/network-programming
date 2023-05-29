d7b7867ee27880a42cabc89dab76460041c76b3a
Timo Vleer <115629806+TimoV2003@users.noreply.github.com>
2023-05-29 13:00:51 +0200

    Chess

---
+++ src/Minigames/chess/Chess.java
@@ -0,0 +1,26 @@
+package Minigames.chess;//Author: ^-^ Veerle ^-^
+import javax.swing.JFrame;
+
+//Chess piece images from http://world.std.com/~wij/fixation/chess-sets.html
+
+public class Chess extends JFrame
+{
+		
+	public void launch() //With applications, you have to specify a main method (not with applets)
+	{
+		JFrame.setDefaultLookAndFeelDecorated(true); //Make it look nice
+        JFrame frame = new JFrame("Chess Game"); //Title
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        chessGUI chessWindow = new chessGUI();
+        frame.setContentPane(chessWindow.createGUI(frame));
+        frame.addWindowFocusListener(chessWindow);
+        
+        frame.setSize(550,650);
+        frame.setResizable(false);
+        frame.setVisible(true);  
+        frame.pack();       
+        
+    }	
+	
+}
