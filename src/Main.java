09a34f1198a3c92dcbfed873c37a5038451dbff0
Timo Vleer <115629806+TimoV2003@users.noreply.github.com>
2023-05-29 12:21:16 +0200

    Beginning of the application

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+        ObservableList<String> gameOptions = FXCollections.observableArrayList(
+                "Tic Tac Toe",
+                "Chess"
+        );
+        ListView<String> listView = new ListView<>(gameOptions);
 
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                System.out.println("Selected game: " + newValue);
+                // TODO: Launch the selected game or perform any other action
+            }
+        });
+
+        VBox root = new VBox(listView);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Minigames App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(Main.class);
     }
 }
