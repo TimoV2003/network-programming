6d3f9c3b6a44c40bb3ec2a7795d2756ad76cb6ef
Timo Vleer <115629806+TimoV2003@users.noreply.github.com>
2023-05-29 12:01:02 +0200

    The Beginning V2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
