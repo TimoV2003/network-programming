dad9a497898fdae652c347a4376fe46c6dac78dc
Nikulair <niekhoevenaars@gmail.com>
2023-06-30 20:29:39 +0200

    gameserver klasse omgegoid op een goede manier

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -18,71 +18,60 @@ public class GameServer {
             System.out.println("Server started. Listening on port " + PORT);
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-
-
                 if (connectedClients < MAX_CLIENTS) {
+                    Socket clientSocket = serverSocket.accept();
                     connections[connectedClients] = new Connection(clientSocket);
-
-                    // TODO: Handle the client connection, manage game sessions, and communicate with clients
-
-
                     connectedClients++;
-                }
-                if (connectedClients == MAX_CLIENTS) {
-                    System.out.println("Maximum number of clients reached.");
-                    break;
-                }
-            }
+                    
+                } else {
+                    switch (gameServerState) {
+                        case GAME_SELECTION:
+                        System.out.println("Game selection");
+                            if (connections[0].getGameSelected() && connections[1].getGameSelected()) {
+                                System.out.println("Both players selected a game.");
+                                int number = (int) Math.round(Math.random());
+                                connections[0].sendPacket(new Packet.GameInnit(connections[0].getNickName(), connections[1].getNickName(), connections[number].getGame()));
+                                connections[1].sendPacket(new Packet.GameInnit(connections[0].getNickName(), connections[1].getNickName(), connections[number].getGame()));
 
-            while (true) {
-                switch (gameServerState) {
-                    case GAME_SELECTION:
-//                        System.out.println("Game selection");
-                        if (connections[0].getGameSelected() && connections[1].getGameSelected()) {
-                            System.out.println("Both players selected a game.");
-                            int number = (int) Math.round(Math.random());
-                            connections[0].sendPacket(new Packet.GameInnit(connections[0].getNickName(), connections[1].getNickName(), connections[number].getGame()));
-                            connections[1].sendPacket(new Packet.GameInnit(connections[0].getNickName(), connections[1].getNickName(), connections[number].getGame()));
+                                connections[0].setGameSelected(false);
+                                connections[1].setGameSelected(false);
 
-                            connections[0].setGameSelected(false);
-                            connections[1].setGameSelected(false);
+                                switch (connections[number].getGame()) {
+                                    case CHESS:
+                                        System.out.println("Chess");
+                                        gameServerState = GameServerState.GAME_CHESS;
 
-                            switch (connections[number].getGame()) {
-                                case CHESS:
-                                    System.out.println("Chess");
-                                    gameServerState = GameServerState.GAME_CHESS;
-
-                                    // todo: start chess game
-                                    break;
-                                case TIC_TAC_TOE:
-                                    System.out.println("TicTacToe");
-                                    gameServerState = GameServerState.GAME_TIC_TAC_TOE;
-                                    // todo: start tic tac toe game
-                                    break;
-                                case CONNECT_FOUR:
-                                    System.out.println("ConnectFour");
-                                    gameServerState = GameServerState.GAME_CONNECT_FOUR;
-                                    // todo: start connect four game
-                                    break;
+                                        // todo: start chess game
+                                        break;
+                                    case TIC_TAC_TOE:
+                                        System.out.println("TicTacToe");
+                                        gameServerState = GameServerState.GAME_TIC_TAC_TOE;
+                                        // todo: start tic tac toe game
+                                        break;
+                                    case CONNECT_FOUR:
+                                        System.out.println("ConnectFour");
+                                        gameServerState = GameServerState.GAME_CONNECT_FOUR;
+                                        // todo: start connect four game
+                                        break;
+                                }
                             }
-                        }
-                        break;
-                    case GAME_CHESS:
+                            break;
+                        case GAME_CHESS:
 
-                        break;
-                    case GAME_TIC_TAC_TOE:
+                            break;
+                        case GAME_TIC_TAC_TOE:
 
-                        break;
-                    case GAME_CONNECT_FOUR:
+                            break;
+                        case GAME_CONNECT_FOUR:
 
-                        break;
-                }
+                            break;
+                    }
 
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
 
             }
